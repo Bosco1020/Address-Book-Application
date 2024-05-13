46fdb983eae2cfb43816d8b26e0352ec0c860d1b
[Connor Boscherini] <connorbos@hotmail.co.uk>
2024-05-13 14:34:27 +0100

    US1-T1: Test written and passed successfully

--- src/main/java/com/digitalfutures/app/Contact.java
+++ src/main/java/com/digitalfutures/app/Contact.java
@@ -1,4 +1,27 @@
 package com.digitalfutures.app;
 
 public class Contact {
+
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String newName, String newPhone, String newEmail) {
+        this.name = newName;
+        this.phoneNumber = newPhone;
+        this.email = newEmail;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
--- src/test/java/com/digitalfutures/app/ContactTest.java
+++ src/test/java/com/digitalfutures/app/ContactTest.java
@@ -12,14 +12,21 @@ public class ContactTest {
     @DisplayName("Constructor tests")
     class ConstructorTests {
 
+        // US1: Test 1
         @Test
-        @DisplayName("Constructor sets expected values when valid")
-        public void constructorSetsValuesWhenValid() {
+        @DisplayName("Constructor takes arguments for a contacts name, phone number & email and set those value")
+        public void constructorTakesNameNumberEmailAndSetsValues() {
             // Arrange
-
+            String name = "Sam";
+            String phoneNumber = "07123456789";
+            String email = "sam@aol.com";
             // Act
-
+            Contact b = new Contact(name, phoneNumber, email);
             //Assert
+            assertAll("Constructor set values to inputs",
+                    () -> assertEquals(name, b.getName()),
+                    () -> assertEquals(phoneNumber, b.getPhoneNumber()),
+                    () -> assertEquals(email, b.getEmail()));
         }
     }
 
