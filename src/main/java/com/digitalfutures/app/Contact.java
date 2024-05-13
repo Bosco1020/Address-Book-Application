5262181ecacfc4fd12ee6ba4674a3dd4de67be6c
[Connor Boscherini] <connorbos@hotmail.co.uk>
2024-05-13 14:22:36 +0100

    NEW: set up pom.xml with jacoco & prepare testing space

---
+++ src/main/java/com/digitalfutures/app/Contact.java
@@ -0,0 +1,4 @@
+package com.digitalfutures.app;
+
+public class Contact {
+}
---
+++ src/test/java/com/digitalfutures/app/ContactTest.java
@@ -0,0 +1,26 @@
+package com.digitalfutures.app;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContactTest {
+
+    @Nested
+    @DisplayName("Constructor tests")
+    class ConstructorTests {
+
+        @Test
+        @DisplayName("Constructor sets expected values when valid")
+        public void constructorSetsValuesWhenValid() {
+            // Arrange
+
+            // Act
+
+            //Assert
+        }
+    }
+
+}
